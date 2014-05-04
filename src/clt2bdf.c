95d0b125d77d3ff401dc1a0c6e98de7570cb5435
Jan Engelhardt <jengelh@inai.de>
2014-05-04 02:57:47 +0200

    clt2bdf: add SFD output for FontForge

--- src/clt2bdf.c
+++ src/clt2bdf.c
@@ -20,6 +20,11 @@
 #include <libHX/option.h>
 #include <libHX/string.h>
 
+enum {
+	FT_SFD = 0,
+	FT_BDF,
+};
+
 /**
  * width-height union
  */
@@ -45,10 +50,138 @@ struct bdf_handle {
 	long header_length;
 	union wh bbox;
 	unsigned int descent, ascent, xheight, capheight, num_glyphs;
+
+	void (*emit_header)(struct bdf_handle *);
+	void (*close)(struct bdf_handle *);
 };
 
 static char *p_output_file;
 static unsigned int p_descent, p_xheight = UINT_MAX;
+static unsigned int p_output_filetype;
+
+static struct bdf_handle *generic_open(const char *file)
+{
+	struct bdf_handle *f = malloc(sizeof(*f));
+
+	if (f == NULL) {
+		perror("malloc");
+		return NULL;
+	}
+	f->handle = fopen(file, "w");
+	if (f->handle == NULL) {
+		int ret = errno;
+		fprintf(stderr, "ERROR: Could not open %s for writing: %s\n",
+		        file, strerror(errno));
+		free(f);
+		errno = ret;
+		return NULL;
+	}
+	f->bbox.width = f->bbox.height = 0;
+	f->descent = p_descent;
+	f->ascent = UINT_MAX;
+	f->xheight = p_xheight;
+	f->capheight = UINT_MAX;
+	f->num_glyphs = UINT_MAX;
+	return f;
+}
+
+static void generic_close(struct bdf_handle *bdf)
+{
+	long of;
+
+	if (bdf->ascent == UINT_MAX)
+		bdf->ascent = bdf->bbox.height - bdf->descent;
+	if (bdf->capheight == UINT_MAX)
+		bdf->capheight = bdf->ascent;
+	if (bdf->xheight == UINT_MAX)
+		bdf->xheight = bdf->ascent / 2;
+	of = fseek(bdf->handle, 0, SEEK_SET);
+	if (of < 0) {
+		perror("fseek");
+		abort();
+	}
+	while (bdf->header_length-- > 0)
+		fputc('\n', bdf->handle);
+	of = fseek(bdf->handle, 0, SEEK_SET);
+	if (of < 0) {
+		perror("fseek");
+		abort();
+	}
+	bdf->emit_header(bdf);
+	fclose(bdf->handle);
+	free(bdf);
+}
+
+static void sfd_emit(struct bdf_handle *font)
+{
+	FILE *f = font->handle;
+	fprintf(f,
+		"SplineFontDB: 3.0\n"
+		"FontName: newfont\n"
+		"FullName: New Font\n"
+		"FamilyName: newfont\n"
+		"Weight: Medium\n"
+		"Copyright: created by clt2bdf/clt2sfd\n"
+		"UComments: created by clt2bdf/clt2sfd\n"
+		"Version: 001.000\n"
+		"ItalicAngle: 0\n"
+		"UnderlinePosition: -100\n"
+		"UnderlineWidth: 40\n"
+	);
+	fprintf(f,
+		"Ascent: %d\n"
+		"Descent: %d\n",
+		font->ascent, font->descent
+	);
+	fprintf(f,
+		"NeedsXUIDChange: 1\n"
+		"FSType: 0\n"
+		"PfmFamily: 33\n"
+		"TTFWeight: 500\n"
+		"TTFWidth: 5\n"
+		"Panose: 2 0 6 4 0 0 0 0 0 0\n"
+		"LineGap: 72\n"
+		"VLineGap: 0\n"
+		"OS2WinAscent: 0\n"
+		"OS2WinAOffset: 1\n"
+		"OS2WinDescent: 0\n"
+		"OS2WinDOffset: 1\n"
+		"HheadAscent: 0\n"
+		"HheadAOffset: 1\n"
+		"HheadDescent: 0\n"
+		"HheadDOffset: 1\n"
+		"Encoding: UnicodeBmp\n"
+		"UnicodeInterp: none\n"
+		"DisplaySize: -24\n"
+		"AntiAlias: 1\n"
+		"FitToEm: 1\n"
+		"WinInfo: 0 50 22\n"
+		"TeXData: 1 0 0 346030 173015 115343 0 1048576 115343 783286 444596 497025 792723 393216 433062 380633 303038 157286 324010 404750 52429 2506097 1059062 262144\n"
+	);
+	fprintf(f, "BeginChars: 65536 %d\n", font->num_glyphs);
+}
+
+static void sfd_close(struct bdf_handle *font)
+{
+	fprintf(font->handle, "EndChars\nEndSplineFont\n");
+	generic_close(font);
+}
+
+static struct bdf_handle *sfd_open(const char *file)
+{
+	struct bdf_handle *fh = generic_open(file);
+
+	if (fh == NULL)
+		return NULL;
+	fh->emit_header = sfd_emit;
+	fh->close = sfd_close;
+
+	fh->emit_header(fh);
+	fprintf(fh->handle, "%-*s", 80, "");
+	fh->header_length = ftell(fh->handle);
+	fh->num_glyphs = 0;
+	return fh;
+}
 
 /**
  * @bdf:	in-memory BDF header structure
@@ -83,7 +216,6 @@ static void bdf_emit(struct bdf_handle *bdf)
 		"ENDPROPERTIES\n",
 		bdf->bbox.width, bdf->xheight);
 	fprintf(f, "CHARS %u\n", bdf->num_glyphs);
-
 }
 
 /**
@@ -94,58 +226,16 @@ static void bdf_emit(struct bdf_handle *bdf)
  */
 static struct bdf_handle *bdf_open(const char *file)
 {
-	struct bdf_handle *bdf = malloc(sizeof(*bdf));
-
-	if (bdf == NULL) {
-		perror("malloc");
-		assert(bdf != NULL);
-	}
-	bdf->handle = fopen(file, "w");
-	if (bdf->handle == NULL) {
-		int ret = errno;
-		fprintf(stderr, "ERROR: Could not open %s for writing: %s\n",
-		        file, strerror(errno));
-		free(bdf);
-		errno = ret;
+	struct bdf_handle *bdf = generic_open(file);
+	if (bdf == NULL)
 		return NULL;
-	}
-	bdf->bbox.width = bdf->bbox.height = 0;
-	bdf->descent = p_descent;
-	bdf->ascent = UINT_MAX;
-	bdf->xheight = p_xheight;
-	bdf->capheight = UINT_MAX;
-	bdf->num_glyphs = UINT_MAX;
-	bdf_emit(bdf);
+	bdf->emit_header = bdf_emit;
+	bdf->close = generic_close;
+
+	bdf->emit_header(bdf);
 	bdf->header_length = ftell(bdf->handle);
 	bdf->num_glyphs = 0;
 	return bdf;
-}
-
-static void bdf_close(struct bdf_handle *bdf)
-{
-	long of;
-
-	if (bdf->ascent == UINT_MAX)
-		bdf->ascent = bdf->bbox.height - bdf->descent;
-	if (bdf->capheight == UINT_MAX)
-		bdf->capheight = bdf->ascent;
-	if (bdf->xheight == UINT_MAX)
-		bdf->xheight = bdf->ascent / 2;
-	of = fseek(bdf->handle, 0, SEEK_SET);
-	if (of < 0) {
-		perror("fseek");
-		abort();
-	}
-	while (bdf->header_length-- > 0)
-		fputc('\n', bdf->handle);
-	of = fseek(bdf->handle, 0, SEEK_SET);
-	if (of < 0) {
-		perror("fseek");
-		abort();
-	}
-	bdf_emit(bdf);
-	fclose(bdf->handle);
-	free(bdf);
 }
 
 /**
@@ -193,39 +283,69 @@ static int p_process_glyph_file(struct bdf_handle *bdf, const char *glyph_file)
 	if (glyph_height > bdf->bbox.height)
 		bdf->bbox.height = glyph_height;
 
-	fprintf(fp, "STARTCHAR U+%04x\n", glyph_number);
-	fprintf(fp, "ENCODING %d\n", glyph_number);
-	fprintf(fp, "SWIDTH %u 0\n", glyph_width * 1000);
-	fprintf(fp, "DWIDTH %u 0\n", glyph_width);
-	fprintf(fp, "BBX %u %u 0 %u\n", glyph_width, glyph_height, bdf->descent);
-	fprintf(fp, "BITMAP\n");
+	if (p_output_filetype == FT_SFD) {
+		int y = glyph_height - 1 - bdf->descent;
 
-	while (HX_getl(&line, glyph_handle) != NULL) {
-		const char *x = line;
-		unsigned int bit = 0, pixel = 0;
+		fprintf(fp, "StartChar: %04x\n", glyph_number);
+		fprintf(fp, "Encoding: %d %d %d\n", glyph_number, glyph_number, glyph_number);
+		fprintf(fp, "Width: %d\n", glyph_width);
+		fprintf(fp, "Flags: H\n");
+		fprintf(fp, "TeX: 0 0 0 0\n");
+		fprintf(fp, "Fore\n");
 
-		HX_chomp(line);
-		for (x = line; x[0] != '\0' && x[1] != '\0'; x += 2) {
-			if ((x[0] == '.' || HX_isspace(x[0])) &&
-			    (x[1] == '.' || HX_isspace(x[1])))
-				/* whitespace */;
-			else
-				pixel |= 1 << (7 - bit);
-			if (bit++ != 8)
-				continue;
-			fprintf(fp, "%02x", pixel);
-			pixel = 0;
-			bit = 0;
+		while (HX_getl(&line, glyph_handle) != NULL) {
+			const char *p = line;
+			unsigned int x;
+			for (x = 0, p = line; p[0] != '\0' && p[1] != '\0';
+			     ++x, p += 2)
+			{
+				if ((p[0] == '.' || HX_isspace(p[0])) &&
+				    (p[1] == '.' || HX_isspace(p[1])))
+					continue;
+				fprintf(fp, "%d %d m 25\n", x, y);
+				fprintf(fp, " %d %d l 25\n", x, y + 1);
+				fprintf(fp, " %d %d l 25\n", x + 1, y + 1);
+				fprintf(fp, " %d %d l 25\n", x + 1, y);
+				fprintf(fp, " %d %d l 25\n", x, y);
+			}
+			--y;
 		}
-		if (bit > 0) {
-			fprintf(fp, "%02x", pixel);
-			pixel = 0;
-			bit = 0;
+		fprintf(fp, "EndSplineSet\nEndChar\n");
+	} else if (p_output_filetype == FT_BDF) {
+		fprintf(fp, "STARTCHAR U+%04x\n", glyph_number);
+		fprintf(fp, "ENCODING %d\n", glyph_number);
+		fprintf(fp, "SWIDTH %u 0\n", glyph_width * 1000);
+		fprintf(fp, "DWIDTH %u 0\n", glyph_width);
+		fprintf(fp, "BBX %u %u 0 %u\n", glyph_width, glyph_height, bdf->descent);
+		fprintf(fp, "BITMAP\n");
+
+		while (HX_getl(&line, glyph_handle) != NULL) {
+			const char *x = line;
+			unsigned int bit = 0, pixel = 0;
+
+			HX_chomp(line);
+			for (x = line; x[0] != '\0' && x[1] != '\0'; x += 2) {
+				if ((x[0] == '.' || HX_isspace(x[0])) &&
+				    (x[1] == '.' || HX_isspace(x[1])))
+					/* whitespace */;
+				else
+					pixel |= 1 << (7 - bit);
+				if (bit++ != 8)
+					continue;
+				fprintf(fp, "%02x", pixel);
+				pixel = 0;
+				bit = 0;
+			}
+			if (bit > 0) {
+				fprintf(fp, "%02x", pixel);
+				pixel = 0;
+				bit = 0;
+			}
+			fprintf(fp, "\n");
 		}
-		fprintf(fp, "\n");
+
+		fprintf(fp, "ENDCHAR\n");
 	}
-
-	fprintf(fp, "ENDCHAR\n");
 	fclose(glyph_handle);
 	HXmc_free(line);
 	return 1;
@@ -238,8 +358,15 @@ p_process_filemap(struct bdf_handle *bdf, const struct HXmap *filemap)
 	const struct HXmap_node *node;
 	int ret = EXIT_SUCCESS;
 
-	while ((node = HXmap_traverse(trav)) != NULL)
+	while ((node = HXmap_traverse(trav)) != NULL) {
 		ret = p_process_glyph_file(bdf, node->data);
+		if (ret <= 0) {
+			ret = EXIT_FAILURE;
+			break;
+		} else {
+			ret = EXIT_SUCCESS;
+		}
+	}
 
 	HXmap_travfree(trav);
 	return ret;
@@ -291,6 +418,10 @@ static int p_collect_files(struct HXmap *map, const char *dir_name)
 static bool p_get_options(int *argc, const char ***argv)
 {
 	static const struct HXoption options_table[] = {
+		{.ln = "bdf", .type = HXTYPE_VAL, .ptr = &p_output_filetype,
+		 .val = FT_BDF, .help = "Generate BDF output (for gbdfed, bdftopcf)"},
+		{.ln = "sfd", .type = HXTYPE_VAL, .ptr = &p_output_filetype,
+		 .val = FT_SFD, .help = "Generate SFD output (for Fontforge)"},
 		{.sh = 'd', .type = HXTYPE_UINT, .ptr = &p_descent,
 		 .help = "Set the font's descent"},
 		{.sh = 'o', .type = HXTYPE_STRING, .ptr = &p_output_file,
@@ -301,6 +432,7 @@ static bool p_get_options(int *argc, const char ***argv)
 		HXOPT_TABLEEND,
 	};
 
+	p_output_filetype = FT_SFD;
 	if (HX_getopt(options_table, argc, argv, HXOPT_USAGEONERR) !=
 	    HXOPT_ERR_SUCCESS)
 		return false;
@@ -332,13 +464,16 @@ int main(int argc, const char **argv)
 			goto out;
 	}
 
-	bdf = bdf_open(p_output_file);
+	if (p_output_filetype == FT_SFD)
+		bdf = sfd_open(p_output_file);
+	else if (p_output_filetype == FT_BDF)
+		bdf = bdf_open(p_output_file);
 	if (bdf == NULL)
 		goto out;
 	ret = p_process_filemap(bdf, filemap);
  out:
 	HXmap_free(filemap);
 	if (bdf != NULL)
-		bdf_close(bdf);
+		bdf->close(bdf);
 	return ret;
 }
