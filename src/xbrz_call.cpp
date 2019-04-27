72781ed5e5369005268149485270ecc594ad941a
Jan Engelhardt <jengelh@inai.de>
2019-04-28 00:47:31 +0200

    cltxscale: update for xBRZ 1.1/HqMAME 2.25

--- src/xbrz_call.cpp
+++ src/xbrz_call.cpp
@@ -5,7 +5,7 @@
 void xbrz_scale(size_t factor, const uint32_t *src, uint32_t *trg,
     unsigned int src_width, unsigned int src_height)
 {
-	xbrz::scale(factor, src, trg, src_width, src_height);
+	xbrz::scale(factor, src, trg, src_width, src_height, xbrz::ColorFormat::RGB);
 }
 
 void xbrz_nearest(size_t factor, const uint32_t *src, uint32_t *trg,
