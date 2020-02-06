735543087aaee5522d4c6f6ce7a1a939046a063e
Jan Engelhardt <jengelh@inai.de>
2020-02-06 16:57:51 +0100

    vfalib: catch cases of broken edge generation

--- src/vfalib.cpp
+++ src/vfalib.cpp
@@ -751,6 +751,11 @@ void vectorizer::finalize()
 	 */
 	for (auto edge = emap.begin(); edge != emap.end(); ) {
 		auto twin = emap.find({edge->end_vtx, edge->start_vtx});
+		if (twin == edge) {
+			printf("Glyph outline description is faulty: edge with startvtx==endvtx (%d,%d)\n",
+				edge->start_vtx.x, edge->start_vtx.y);
+			break;
+		}
 		if (twin == emap.cend()) {
 			++edge;
 			continue;
